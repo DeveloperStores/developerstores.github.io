cadedd17b2d0323136203481026c7d77069689b8
DeveloperStores <vanshabc5132010@gmail.com>
2026-06-11 20:55:56 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,27 +1,15 @@
-const { Telegraf, Markup } = require('telegraf');
+import { Telegraf, Markup } from 'telegraf';
 
-// Aapka Bot Token
 const BOT_TOKEN = '8245593482:AAHmwHg3lN-cWtl6BVFoGextCXzQ_PbVHLc';
-
 const bot = new Telegraf(BOT_TOKEN);
 
-// Jab user /start command bhejega
 bot.start((ctx) => {
-    ctx.reply(
-        'Welcome! MindIQ Tester mein aapka swagat hai. Continue karne ke liye niche diye gaye button par click karein:',
+    ctx.reply('Welcome! Continue karne ke liye button par click karein:', 
         Markup.inlineKeyboard([
-            [
-                Markup.button.url('Continue', 'https://mindiqtester.github.io')
-            ]
+            [Markup.button.url('Continue', 'https://mindiqtester.github.io')]
         ])
     );
 });
 
-// Bot start karein
-bot.launch()
-    .then(() => console.log('✅ Bot bina kisi crash ke chal gaya hai!'))
-    .catch((err) => console.error('❌ Bot launch karne mein error aaya:', err));
-
-// Graceful stop
-process.once('SIGINT', () => bot.stop('SIGINT'));
-process.once('SIGTERM', () => bot.stop('SIGTERM'));
+bot.launch();
+console.log('Bot is running...');
